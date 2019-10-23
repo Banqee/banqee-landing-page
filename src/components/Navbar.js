417eae7f8bc4f661a874b1dbbfcabd3b8659aab7
Elektropay <richard.r@everpayinc.com>
2019-10-23 03:40:47 -0500

    2nd commit
    chnaged logo width

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -42,7 +42,7 @@ const Navbar = class extends React.Component {
         <div className="container">
           <div className="navbar-brand">
             <Link to="/" className="navbar-item" title="Logo">
-              <img src={logo} alt="Banqee" style={{ width: '88px' }} />
+              <img src={logo} alt="Banqee" style={{ width: '138px' }} />
             </Link>
             {/* Hamburger menu */}
             <div
