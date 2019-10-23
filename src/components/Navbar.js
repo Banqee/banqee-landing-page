47b08522454a5b4d673cbddd2b43a8d06d616df0
Elektropay <richard.r@everpayinc.com>
2019-10-23 03:45:22 -0500

    changed logo size
    changed logo size

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -42,7 +42,7 @@ const Navbar = class extends React.Component {
         <div className="container">
           <div className="navbar-brand">
             <Link to="/" className="navbar-item" title="Logo">
-              <img src={logo} alt="Banqee" style={{ width: '138px' }} />
+              <img src={logo} alt="Banqee" style={{ width: '98px' }} />
             </Link>
             {/* Hamburger menu */}
             <div
