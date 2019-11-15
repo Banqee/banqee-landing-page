ae14986addb921740e17b48ccc19ab5f973721f2
Elektropay <richard.r@everpayinc.com>
2019-11-15 12:33:13 -0600

    Update Layout.js

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -14,7 +14,7 @@ const TemplateWrapper = ({ children }) => {
         <html lang="en" />
         <title>{title}</title>
         <meta name="description" content={description} />
-
+        <base href="/">
         <link
           rel="apple-touch-icon"
           sizes="180x180"
