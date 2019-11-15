962699adbef86d03c7b0e729749554846f125cf7
Elektropay <richard.r@everpayinc.com>
2019-11-15 01:35:13 -0600

    Update index-page.js

--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import { Image } from 'cloudinary'
 import { Link, graphql } from 'gatsby'
-
 import Layout from '../components/Layout'
 import Features from '../components/Features'
 import BlogRoll from '../components/BlogRoll'
@@ -188,7 +188,9 @@ export const IndexPageTemplate = ({
   >
   <div className="iphone-x light front">
   <div className="screen shadow-box">
+<Image publicId="Banqee/iphone-display.png" >
 
+</Image>
   </div>
   <div 
   className="notch"
