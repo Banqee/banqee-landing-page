ff4949cccd54a2addab321d71d26b8843af9617d
Elektropay <richard.r@everpayinc.com>
2019-11-16 00:22:51 -0600

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -32,6 +32,20 @@ module.exports = {
     {
       resolve: 'gatsby-source-filesystem',
       options: {
+        path: `${__dirname}/src/css/vendor-min.css`,
+        name: 'stylesheet',
+      },
+    },
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        path: `${__dirname}/src/css/banqee-min.css`,
+        name: 'stylesheet',
+      },
+    },
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
         path: `${__dirname}/src/js`,
         name: 'javascript',
       },
@@ -82,7 +96,7 @@ module.exports = {
     {
       resolve: 'gatsby-plugin-purgecss', // purges all unused/unreferenced css rules
       options: {
-        develop: true, // Activates purging in npm run develop
+        develop: false, // Activates purging in npm run develop
         purgeOnly: ['/all.sass'], // applies purging only on the bulma css file
       },
     }, // must be after other CSS plugins
